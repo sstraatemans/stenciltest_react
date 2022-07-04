31d7cf6d94eb930b323f95a6f080cc6dbf79d84b
Steven Straatemans <steven@straatemans.nl>
2022-07-04 11:23:48 +0200

    init commit

---
+++ dist/components/stencil-generated/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+import { defineCustomElements } from 'stenciltest/loader';
+defineCustomElements();
+export const MrvlButton = /*@__PURE__*/ createReactComponent('mrvl-button');
+export const MrvlCheckbox = /*@__PURE__*/ createReactComponent('mrvl-checkbox');
+export const MrvlForm = /*@__PURE__*/ createReactComponent('mrvl-form');
+export const MrvlInput = /*@__PURE__*/ createReactComponent('mrvl-input');
+export const MyComponent = /*@__PURE__*/ createReactComponent('my-component');
+//# sourceMappingURL=index.js.map
--- src/components/stencil-generated/index.ts
+++ src/components/stencil-generated/index.ts
@@ -1,30 +1,15 @@
-/* 
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
 
-auto-generated react proxies will replace this file 
-if you use "../your-react-library-name/src/components/stencil-generated/index.ts" for your proxies target:
+import type { JSX } from 'stenciltest';
 
-*/
+import { defineCustomElements } from 'stenciltest/loader';
 
-/* SAMPLE: 
-import { reactOutputTarget as react } from '@stencil/react-output-target'
-
-export const config: Config = {
-  outputTargets: [
-    react({
-      componentCorePackage: 'your-stencil-library-name',
-      proxiesFile: '../your-react-library-name/src/components/stencil-generated/index.ts',
-      includeDefineCustomElements: true,
-    });
-    {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-    },
-    {
-      type: 'dist-custom-elements',
-      dir: "./dist/custom-elements"
-    },
-  ],
-}
-*/
-
-export const object = {};
+defineCustomElements();
+export const MrvlButton = /*@__PURE__*/createReactComponent<JSX.MrvlButton, HTMLMrvlButtonElement>('mrvl-button');
+export const MrvlCheckbox = /*@__PURE__*/createReactComponent<JSX.MrvlCheckbox, HTMLMrvlCheckboxElement>('mrvl-checkbox');
+export const MrvlForm = /*@__PURE__*/createReactComponent<JSX.MrvlForm, HTMLMrvlFormElement>('mrvl-form');
+export const MrvlInput = /*@__PURE__*/createReactComponent<JSX.MrvlInput, HTMLMrvlInputElement>('mrvl-input');
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
