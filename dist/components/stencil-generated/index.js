52604fcf8370af3f26256388cb52c8c946d924cb
Steven Straatemans <steven@straatemans.nl>
2022-07-06 12:07:57 +0200

    generated components

--- dist/components/stencil-generated/index.js
+++ dist/components/stencil-generated/index.js
@@ -5,8 +5,10 @@ import { createReactComponent } from './react-component-lib';
 import { defineCustomElements } from 'stenciltest/loader';
 defineCustomElements();
 export const MrvlButton = /*@__PURE__*/ createReactComponent('mrvl-button');
+export const MrvlCard = /*@__PURE__*/ createReactComponent('mrvl-card');
 export const MrvlCheckbox = /*@__PURE__*/ createReactComponent('mrvl-checkbox');
 export const MrvlForm = /*@__PURE__*/ createReactComponent('mrvl-form');
 export const MrvlInput = /*@__PURE__*/ createReactComponent('mrvl-input');
+export const MrvlTypography = /*@__PURE__*/ createReactComponent('mrvl-typography');
 export const MyComponent = /*@__PURE__*/ createReactComponent('my-component');
 //# sourceMappingURL=index.js.map
--- src/components/stencil-generated/index.ts
+++ src/components/stencil-generated/index.ts
@@ -9,7 +9,9 @@ import { defineCustomElements } from 'stenciltest/loader';
 
 defineCustomElements();
 export const MrvlButton = /*@__PURE__*/createReactComponent<JSX.MrvlButton, HTMLMrvlButtonElement>('mrvl-button');
+export const MrvlCard = /*@__PURE__*/createReactComponent<JSX.MrvlCard, HTMLMrvlCardElement>('mrvl-card');
 export const MrvlCheckbox = /*@__PURE__*/createReactComponent<JSX.MrvlCheckbox, HTMLMrvlCheckboxElement>('mrvl-checkbox');
 export const MrvlForm = /*@__PURE__*/createReactComponent<JSX.MrvlForm, HTMLMrvlFormElement>('mrvl-form');
 export const MrvlInput = /*@__PURE__*/createReactComponent<JSX.MrvlInput, HTMLMrvlInputElement>('mrvl-input');
+export const MrvlTypography = /*@__PURE__*/createReactComponent<JSX.MrvlTypography, HTMLMrvlTypographyElement>('mrvl-typography');
 export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
